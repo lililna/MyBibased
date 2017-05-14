537974dbf5ee3df1d9dc9d9e9ba7fa6c47e22337
lililna <1423716128@qq.com>
2017-05-14 17:07:18 +0800

    部分bug修改和答辩PPT

--- public/javascripts/naconfirm.js
+++ public/javascripts/naconfirm.js
@@ -5,16 +5,19 @@ var goodsimg =details.goodsimg;
 var price = details.price;
 var count = details.count;
 var danjia = price.substr(1);
-//alert(danjia)
-		$(function(){
-			var str = 	'<p class="shuoming">'+name+'</p>'+
-				'<p class="gdetl">'+
-					'<img src="../'+goodsimg+'"/>'+
-						'<span>价格：<b id="price">'+price+'</b></span><br />'+
-					'<span>数量：<b id="count">'+count+'</b></span>'+
-					
-				'</p>';
-			$(".goods").append(str);
-			$("#zong").text(danjia*count);
-		})
+$(function(){
+	var str = 	'<p class="shuoming">'+name+'</p>'+
+		'<p class="gdetl">'+
+			'<img src="..'+goodsimg+'"/>'+
+				'<span>价格：<b id="price">'+price+'</b></span><br />'+
+			'<span>数量：<b id="count">'+count+'</b></span>'+
+			
+		'</p>';
+	$(".goods").append(str);
+	$("#zong").text(danjia*count);
+	
+	$(".xinxi").click(function(){
+		window.location.href = "/dizhi";
+	})
+})
 
